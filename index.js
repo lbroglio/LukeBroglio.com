ad4f695d650450a4cdb9e7c72ec6b785748fcb75
lbroglio <lbroglio@iastate.edu>
2023-09-01 13:17:51 -0500

    Converting project page to modular so more can be added dynamically with js. Has start of js function to generate and empty div added to project html. Also included info on 327 game and spamgram project being added

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ function cycleProjects(direction){
     //Removes the active project class from the old active project
     activeProject.classList.remove("activeProject");
 
-    //Gets the of the old active project
     var aProjIndex = 0;
 
     //Get the index of the active project
@@ -68,6 +67,9 @@ function cycleProjects(direction){
 }
 
 function loadProjectIcons(projects){
+    //Convert projects to list of values
+    projects = Object.values(projects)
+
     //Get the list of project cyclers
     var cyclers = document.getElementsByClassName("projectCycler");
 
@@ -107,7 +109,7 @@ function loadProjectIcons(projects){
 
             //Set the src and alt for the image
             currImg.src = currProject.previewImage;
-            currImg.alt = "Project Preview";
+            currImg.alt = currProject.altText;
 
             //Add image to the anchor
             currAnchor.appendChild(currImg);
@@ -135,6 +137,9 @@ function loadProjectIcons(projects){
 
 
 function loadNavbarDropDown(projects){
+    //Convert projects to list of values
+    projects = Object.values(projects)
+
     //Get the list of all navbar dropsdowns
     var dropDowns = document.getElementsByClassName("dropdown-menu");
 
@@ -142,7 +147,7 @@ function loadNavbarDropDown(projects){
     for(var i=0; i < dropDowns.length; i++){
         var currDrop = dropDowns[i];
 
-        //For rvery project
+        //For every project
         for(var j=0; j < projects.length; j++){
             var currProject = projects[j];
 
@@ -164,7 +169,6 @@ function loadNavbarDropDown(projects){
            projectEntry.appendChild(projectAnchor);
 
            //Add list element to dropdowm
-
            currDrop.appendChild(projectEntry)
         }
     }
@@ -172,11 +176,146 @@ function loadNavbarDropDown(projects){
 
 }
 
+function expandImages(id){
+    var moreImages = document.getElementById(id+"ExtraImages");
+
+    moreImages.style.display = "flex";
+
+    var projectCon = document.getElementById(id+"Con");
+
+    projectCon.style.display = "none";
+}
+
+function hideImages(id){
+    var moreImages = document.getElementById(id+"ExtraImages");
+
+    moreImages.style.display = "none";
+
+    var projectCon = document.getElementById(id+"Con");
+
+    projectCon.style.display = "flex";
+}
+
+//Populate the page for a specific project with the data for that project
+//Page is the div to that is the anscestor of all the elements that need to be populated
+//Project data is the information to add retrieved from a JSON  file
+function createProjectPage(page, projectData){
+    //List of children of the page element these are what need to be populated with information
+    children = page.children;
+
+    //ID for this project
+    let id = projectData["id"]
+
+    //Set the title 
+    let projTitle = page.querySelectorAll(".projectTitle")[0]
+    projTitle.setHTML(projectData["name"])
+
+    //Set the id of the page
+    page.id = id
+
+    //Set the id of the project containter
+    let projCon = page.querySelectorAll(".projectCon")[0]
+    projCon.id = id + "Con"
+
+    // Set the id of the div which holds the visuals 
+    let visDiv = page.querySelectorAll(".projectVisuals")[0]
+    visDiv.id = id + "Visuals"
+
+    // Add the image caption to the viusal
+    let caption = document.createElement("h3")
+    caption.setHTML(projectData["desPageImg"]["caption"])
+    visDiv.insertBefore(caption, visDiv.firstChild)
+
+    // Add the image to the visual 
+    let wrapper = document.createElement("div")
+    wrapper.classList.add("imageWrapper")
+    
+    let imgEl = document.createElement("img")
+    imgEl.setAttribute("src",projectData["desPageImg"]["imageSrc"])
+    wrapper.appendChild(imgEl)
+    visDiv.insertBefore(wrapper, visDiv.children[1])
+
+    // Set up the expand button
+    let showButton = visDiv.querySelectorAll(".imageButton")[0]
+    showButton.setAttribute("onclick", "expandImages('"+id+"')")
+
+    // Add the text to the information div 
+    let infoDiv = page.querySelectorAll(".projectInformation")[0]
+
+    //Add the overview text
+    let overviewEl = document.createElement("p")
+    overviewEl.setHTML(projectData["pageText"]["overview"])
+
+    let overviewHeader = infoDiv.querySelectorAll(".projectOverview")[0]
+    overviewHeader.after(overviewEl)
+
+    //Add the details text
+
+    //Create an element to hold a high level description for the details
+    let highLevelEl = document.createElement("p")
+    highLevelEl.setHTML(projectData["pageText"]["details"]["highLevel"])
+    
+    // Create a list holding the details included in the JSON
+    let detailsListEl = document.createElement("ul")
+    let detailsTextList = projectData["pageText"]["details"]["detailList"]
+
+    for(let i =0; i < detailsTextList.length; i++){
+        let tempEL = document.createElement("li")
+        let tempPar = document.createElement("p")
+        tempPar.setHTML(detailsTextList[i])
+        tempEL.appendChild(tempPar)
+        detailsListEl.appendChild(tempEL)
+    }
+    
+
+    // Append the overview and the list after the appropriate header
+    let detailsHeader = infoDiv.querySelectorAll(".projectDetails")[0]
+    detailsHeader.after(highLevelEl)
+    highLevelEl.after(detailsListEl)
+
+    //Create a list with the challenges bullet points
+    let challengeListEL = document.createElement("ul")
+    let challengeTextList =  projectData["pageText"]["challenges"]
+    
+    for(let i=0; i < challengeTextList.length;  i++){
+        let tempEL = document.createElement("li")
+        let tempPar = document.createElement("p")
+        tempPar.setHTML(challengeTextList[i])
+        tempEL.appendChild(tempPar)
+        challengeListEL.appendChild(tempEL)
+    }
+
+    //Append the list after the appropriate header
+    let challengesHeader = infoDiv.querySelectorAll(".projectChallenges")[0]
+    challengesHeader.after(challengeListEL)
+
+    //Set the github link to the proper one according to the project data
+    let githubLink = infoDiv.querySelectorAll(".github-link")[0]
+    githubLink.setAttribute("href", projectData["github-link"])
+
+    //Set up the image gallery
+    let galleryDiv = infoDiv.querySelectorAll("projectImagesFull")
+
+    
+
+    
+}
+
+
+
+var isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/);
+
+if (isMobile) {
+   document.location = "indexM.html";
+}
+
 fetch('./projectInfo.json')
 .then(function (response) {
     return response.json();
 })
 .then(function (data) {
+    let page = document.getElementsByClassName("projectPage")[0]
+    createProjectPage(page, data["i281Asm"])
     loadProjectIcons(data);
     loadNavbarDropDown(data);
 })
