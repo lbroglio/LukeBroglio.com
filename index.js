67e250652de106e86e53c79ab41e1aa7f745b67f
lbroglio <lukebroglio@gmail.com>
2024-10-25 19:48:02 -0500

    Continued work on mobile alternative. Also changed README retrieval code to make less API requests

--- index.js
+++ index.js
@@ -54,23 +54,28 @@ function hideEmail(){
 
 
 // Get the highlighted projects from github and add them to the projects page 
-function getHighlightedProjects() {
+async function getHighlightedProjects(dataCon) {
     // Get JSON
-    fetch("highlighted.json")
+    await fetch("highlighted.json")
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {
+    .then(async function (data) {
+        let highlightedRepos = []
         // Add all projects to HTML
         for(let i=0; i < data.length; i++){
-            setupProjectPage(data[i], "highlightedCon")
+            let tmp = await getReadMe(data[i]);
+            dataCon[data[i]] = tmp;
+            highlightedRepos.push(data[i])
         }
+        dataCon["highlightedProjs"] = highlightedRepos;
     })
+  
 }
 
 // Get the three projects with the most commits on GitHub in the last two weeks and 
 // add them to the 'current' project section
-async function getCurrentProjects(){
+async function getCurrentProjects(dataCon){
     let page = 1;
     let commitCount = {}
     let endEnum = false
@@ -98,17 +103,26 @@ async function getCurrentProjects(){
     // Add all repose to and array and sort it based on their number of commits
     let keysArr = Object.keys(commitCount);
     let repoArr = [...keysArr];
-    
     // Sort the array based on the values in commitCount
     quickSortFromDict(repoArr, 0, repoArr.length - 1, commitCount);
 
+    let currentRepos = []
+    let reposAdded = 0;
+
     // Add the top three repos 
-    for(let i=0; i < 3 && i < repoArr.length; i++){
+    for(let i=0; reposAdded < 3 && i < repoArr.length; i++){
         let proj = repoArr[i];
         startIndex = proj.indexOf("/") + 1
         proj = proj.substring(startIndex)
-        setupProjectPage(proj, "currentCon")
+        let readME = await getReadMe(proj);
+        if(readME != null){
+            reposAdded += 1;
+            dataCon[proj] = readME;
+            currentRepos.push(proj)
+        }
     }
+    dataCon["currentProjs"] = currentRepos;
+    console.log(dataCon)
 }
 
 
@@ -145,8 +159,8 @@ function enumerateEventsResponsePage(data, commitCount){
 }
 
 
-// Add a project to the HTML
-function setupProjectPage(projectName, sectionContainerName){
+// Retrieve a README file for a repository
+async function getReadMe(projectName){
     // Read the names of highlighted projects from JSON file
 
     // Add pages for all of the highlighted projects
@@ -154,6 +168,23 @@ function setupProjectPage(projectName, sectionContainerName){
 
     // Define the API URL
     const apiUrl = ' https://api.github.com/repos/lbroglio/' + projectName +  '/contents/README.md';
+        
+    // Make a GET request to get the README to display on the projects page
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return null;
+    }
+
+}
+
+// Add a project to the HTML
+function setupProjectPage(readme, projectName, sectionContainerName){
 
     // Add page to put this projects info on
     var newPage = document.createElement("div");
@@ -161,63 +192,50 @@ function setupProjectPage(projectName, sectionContainerName){
     newPage.classList.add("snapPoint");
 
     // Make a GET request to get the README to display on the projects page
-    fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+
+    // Decode the base64 encoded markdown
+    var decodedMD = atob(readme.content);
+        
+    // Remove back slash '\' characters because they are needed in the HTML
+    decodedMD = decodedMD.replaceAll(new RegExp("\\\\", 'g'), "")
+
+    // Convert the markdown to HTML
+    var converter = new showdown.Converter();
+    converter.setFlavor('github');
+    html = converter.makeHtml(decodedMD);
+        
+    // Add some header elements to converted html
+     html = "<h1 class=\"projectHeader\">" + projectName + "</h1>" + 
+        "<a class=\"\" href=\"https://github.com/lbroglio/" + projectName + "\">See this project on GitHub</a>" + html;
+
+    // Add converted HTML to page
+    var containerDiv = document.createElement("div")
+    containerDiv.classList.add("projectMDCon")
+    containerDiv.id = projectName;
+    containerDiv.innerHTML += html
+
+    // Change image srcs to point at GitHub
+    newPage.appendChild(containerDiv);
+    var imgTags = newPage.getElementsByTagName("img")
+    for(let i =0; i < imgTags.length; i++){
+        let origSrc = imgTags[i].src;
+        // TODO: Find a more robust way to detect the start of the needed portion
+        let neededStart;
+        if(origSrc.includes("docs")){
+            neededStart = origSrc.indexOf("docs")
         }
-        return response.json();
-    })
-    .then(data => {
-        // Decode the base64 encoded markdown
-        var decodedMD = atob(data.content);
-        
-        // Remove back slash '\' characters because they are needed in the HTML
-        decodedMD = decodedMD.replaceAll(new RegExp("\\\\", 'g'), "")
+        else if(origSrc.includes("Documents"))(
+            neededStart = origSrc.indexOf("Documents")
+        )
+        else{
+            neededStart = 0;
+        }
 
-        // Convert the markdown to HTML
-        var converter = new showdown.Converter();
-        converter.setFlavor('github');
-        html = converter.makeHtml(decodedMD);
-        
-        // Add some header elements to converted html
-        html = "<h1 class=\"projectHeader\">" + projectName + "</h1>" + 
-            "<a class=\"\" href=\"https://github.com/lbroglio/" + projectName + "\">See this project on GitHub</a>" + html;
-
-        // Add converted HTML to page
-        var containerDiv = document.createElement("div")
-        containerDiv.classList.add("projectMDCon")
-        containerDiv.id = projectName;
-        containerDiv.innerHTML += html
-
-        // Change image srcs to point at GitHub
-        newPage.appendChild(containerDiv);
-        var imgTags = newPage.getElementsByTagName("img")
-        //imgTags = newPage.getElementsByTagName("image")
-        for(let i =0; i < imgTags.length; i++){
-            let origSrc = imgTags[i].src;
-            // TODO: Find a more robust way to detect the start of the needed portion
-            let neededStart;
-            if(origSrc.includes("docs")){
-                neededStart = origSrc.indexOf("docs")
-            }
-            else if(origSrc.includes("Documents"))(
-                neededStart = origSrc.indexOf("Documents")
-            )
-            else{
-                neededStart = 0;
-            }
-
-            imgTags[i].src = "https://github.com/lbroglio/" + projectName + "/blob/main/" + origSrc.substring(neededStart) +"?raw=true";
+        imgTags[i].src = "https://github.com/lbroglio/" + projectName + "/blob/main/" + origSrc.substring(neededStart) +"?raw=true";
             
-            // Add class to image to help formatting
-            imgTags[i].classList.add("projectMDImage")
-        }
-
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        // Add class to image to help formatting
+        imgTags[i].classList.add("projectMDImage")
+    }
 
     // Add new page to HTML
     var highlightedCon = document.getElementById(sectionContainerName)
@@ -236,20 +254,61 @@ function setupProjectPage(projectName, sectionContainerName){
     var hp = highlightedCon.getElementsByClassName("projectHomePage")[0];
     var selector = hp.getElementsByClassName("projectSelector")[0];
     selector.appendChild(linkCon)
+
+}
+
+function addHighlightedProjects(){
+    // Get the stored api data from session storage
+    let dataCon = JSON.parse(sessionStorage.getItem("apiData"));
+    // Add the pages
+    for(let i =0; i < dataCon["highlightedProjs"].length; i++){
+        let projName = dataCon["highlightedProjs"][i];
+        setupProjectPage(dataCon[projName], projName,  "highlightedCon");
+    }
+}
+
+function addCurrentProjects(){
+    // Get the stored api data from session storage
+    let dataCon = JSON.parse(sessionStorage.getItem("apiData"));
+
+    // Add the pages
+    for(let i =0; i < dataCon["currentProjs"].length; i++){
+        let projName = dataCon["currentProjs"][i];
+        setupProjectPage(dataCon[projName], projName,  "currentCon");
+    }
+}
+
+// Make API requests for README data and add them to session storage
+async function makeAPIRequests(){
+    console.log("API Requests")
+    let dataCon = {}
+    // Make requests to github API
+    await getHighlightedProjects(dataCon);
+    await getCurrentProjects(dataCon);
+
+    sessionStorage.setItem("apiData", JSON.stringify(dataCon));
 }
 
 
 
+window.onload = async function(){
 
-window.onload = function(){
-    
+    // If the project data isn't already set
+    let dataCon = sessionStorage.getItem("apiData")
+    if(dataCon === null){
+        await makeAPIRequests();
+    }
+
     var isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/);
     // Get current file name
     var locName = location.pathname.split("/").slice(-1).pop()
     console.log(locName)
     if(locName === "index.html" && isMobile){
-        console.log("HERE");
         document.location = "indexM.html";
+    }
+    else if(locName === "projects.html" || locName === "projectsM.html"){
+        addHighlightedProjects();
+        addCurrentProjects();
     }
     
 
